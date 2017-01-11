21865731a836f2891093874982f894caf1521649
Vlad <vlreznik97@gmail.com>
2017-01-11 03:18:40 +0300

    update

---
+++ project.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+#pragma STDC CX_LIMITED_RANGE on
+
+int main(){
+	float complex a;
+	float a1=-4;
+	printf("%f \n",cimag(a));
+	return 0;
+}
